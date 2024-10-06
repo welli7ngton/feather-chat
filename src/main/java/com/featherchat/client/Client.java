b9ef183856ec105aaccecb9a90672517f4b7ff7a
welli7ngton <wellingtonasilva45@gmail.com>
2024-10-06 17:59:04 -0300

    fix: corrige erros de tratamento de exceptions

---
+++ src/main/java/com/featherchat/client/Client.java
@@ -0,0 +1,76 @@
+package src.main.java.com.featherchat.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client implements Runnable {
+  private Socket client;
+  private BufferedReader in;
+  private PrintWriter out;
+  private boolean done = false;
+
+  @Override
+  public void run() {
+    try {
+      this.client = new Socket("localhost", 4444);
+      out = new PrintWriter(client.getOutputStream(), true);
+      in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+      InputHandler inHandler = new InputHandler();
+      Thread t = new Thread(inHandler);
+      t.start();
+
+      String inMessage;
+      while ((inMessage = in.readLine()) != null) {
+        System.out.println(inMessage);
+      }
+    } catch (Exception i) {
+      shutdown();
+    }
+  }
+
+  public void shutdown() {
+    done = true;
+    try {
+      if (in != null)
+        in.close();
+      if (out != null)
+        out.close();
+      if (client != null && !client.isClosed()) {
+        client.close();
+      }
+    } catch (IOException e) {
+      // ignore
+    }
+  }
+
+  class InputHandler implements Runnable {
+    @Override
+    public void run() {
+      try {
+        BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+        while (!done) {
+          String message = inReader.readLine();
+          if (message.equals("/quit")) {
+            inReader.close();
+            shutdown();
+          } else {
+            out.println(message);
+          }
+        }
+      } catch (IOException e) {
+        System.out.println("An error occurred during the process: " + e.toString());
+        shutdown();
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Client client = new Client();
+    Thread clientThread = new Thread(client);
+    clientThread.start();
+  }
+}
